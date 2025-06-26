ac05647ce477cf965de0c9be4169216390b583e5
Matheus <matheussales497@gmail.com>
2025-06-25 23:27:51 -0300

    feat: adiciona funcionalidade de sessão de usuário e cria página de dashboard

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
 const userController = require('./app/controllers/userController');
@@ -6,8 +7,15 @@ const User = require('./app/models/User');
 const app = express();
 const PORT = 3000;
 
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/partials', express.static(path.join(__dirname, 'app', 'views', 'partials')));
+
+app.use(session({
+    secret: 'seuSegredoSuperSecreto',
+    resave: false,
+    saveUninitialized: false
+}));
 
 // Configurar body-parser pra ler dados
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,4 +42,22 @@ app.post('/login', userController.login);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+function requireLogin(req, res, next) {
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+// rota para o dashboard
+app.get('/dashboard', requireLogin, (req, res) => {
+    res.sendFile(path.join(__dirname, 'app', 'views', 'pages', 'dashboard.html'));
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/login');
+    });
 });
